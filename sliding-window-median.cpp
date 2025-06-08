f9c74a98c0c5879037a2c6ed1c3fffcd11766248
alexandervladsemenov <alexandervladsemenov@yahoo.com>
2025-06-08 01:42:53 -0400

    verified the heap

--- sliding-window-median.cpp
+++ sliding-window-median.cpp
@@ -1,1 +1,12 @@
 #include "common.h"
+
+
+
+
+int main() {
+    std::vector<int> test_data = {2,3,4,-1,2,4,-10};
+    maxHeap maxHeap_test(test_data);
+    maxHeap_test.add(5);
+    maxHeap_test.remove(3);
+    return 0;
+}
